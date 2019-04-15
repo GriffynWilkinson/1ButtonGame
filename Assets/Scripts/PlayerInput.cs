b5e571d047c58ba53cfc0343edd1f6d2bb8d0ebd
GriffynWilkinson <38253161+GriffynWilkinson@users.noreply.github.com>
2019-04-15 11:16:56 -0400

    Initial Commit for the Assets folder
    This commit included the initial prototype scene. you can currently use the 'f' key to make your player flip to the roof, or back down to the floor. there is one obstacle that if you hit, you will die. The current Player sprite, and spike sprite are not original images, and will need to be changed.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput  {
+
+    public delegate void HandleButtonPress();
+
+    public event HandleButtonPress HandlePress;
+
+    // checks to see if the buttonn is pressed, and handles events accordingly
+    public void HandleInput()
+    {
+        // for now, our input will be the 'f' key
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (HandlePress != null)
+            {
+                HandlePress();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    PlayerInput m_Input = new PlayerInput();
+    Rigidbody2D m_Body;
+    float m_ForwardSpeed = 300;
+    float m_FlipSpeed = 500;
+    float m_YVelocity = 0;
+    bool isDead = false;
+    
+	// Use this for initialization
+	void Start () {
+        m_Input.HandlePress += FlipPlayer;
+        m_Body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isDead)
+        {
+            MoveForward();
+            m_Input.HandleInput();
+        }
+	}
+
+    void FlipPlayer()
+    {
+        // add velocity to move the player to either the roof or floor
+        m_YVelocity = transform.up.y * m_FlipSpeed * Time.deltaTime;
+
+        // flip the player over
+        transform.Rotate(transform.eulerAngles.x + 180, 0, 0);
+
+    }
+
+    // add a constant forward velocity to the player
+    void MoveForward()
+    {
+        m_Body.velocity = new Vector2(m_ForwardSpeed * Time.deltaTime, m_YVelocity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Floor")
+        {
+            m_YVelocity = 0;
+        }
+
+        if (collision.collider.gameObject.tag == "Spikes")
+        {
+            Die();
+        }
+    }
+
+
+    void Die()
+    {
+        isDead = true;
+
+        m_ForwardSpeed = 0;
+        m_YVelocity = 0;
+
+        m_Body.velocity = new Vector2(m_ForwardSpeed * Time.deltaTime, m_YVelocity);
+
+        m_Body.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+}
