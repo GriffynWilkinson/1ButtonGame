f34dba4f8b1093b2f20eb5b304abea7f4ed9840e
GriffynWilkinson <griffynjwilkinson@gmail.com>
2019-04-19 17:55:39 -0400

    New Project
    When I updated my unity, it broke the project file, so I made a new one, and re added all my scripts. I have made it so that if a block doesnt have another one to the left of it, it automatically gives itself spikes. working on have the player move properly. Currently he gets stuck, and Im not sure what the problem is at the moment

--- Assets/Scripts/CreateSpikes.cs
+++ Assets/Scripts/CreateSpikes.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Used to add spikes to the block if there is nothing infront of it at the start of the game
 public class CreateSpikes : MonoBehaviour
 {
+    public GameObject m_Spikes;
+    float m_Length;
+
     // Start is called before the first frame update
     void Start()
     {
         
+        // this is the length of one side of a block
+        // calculated by the scale of the box collider (doesn't matter what axis we use as they are both the same)
+        // multiplied by the scale of the local transform, incase the object is scaled.
+        m_Length = (gameObject.GetComponent<BoxCollider2D>().size.x) * (transform.localScale.x);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.left, m_Length);
+
+        if (hitInfo.transform == null)
+        {
+            Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+            pos += Vector2.left * m_Length;
+            Instantiate(m_Spikes, pos, Quaternion.identity);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,30 +6,45 @@ public class PlayerMovement : MonoBehaviour {
 
     PlayerInput m_Input = new PlayerInput();
     Rigidbody2D m_Body;
-    float m_ForwardSpeed = 300;
-    float m_FlipSpeed = 500;
+    float m_ForwardSpeed = 75f;
+    float m_FlipSpeed;
     float m_YVelocity = 0;
     bool isDead = false;
-    
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
+        m_FlipSpeed = m_ForwardSpeed * 1.5f;
         m_Input.HandlePress += FlipPlayer;
         m_Body = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (!isDead)
         {
             MoveForward();
             m_Input.HandleInput();
         }
-	}
+    }
 
     void FlipPlayer()
     {
+        // Make sure the player is touching the ground or roof so he can't flip in mid air
+        float length;
+
+        length = (gameObject.GetComponent<BoxCollider2D>().size.y) * (transform.localScale.y);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, length);
+
+        if (hitInfo.transform == null)
+        {
+            return;
+        }
+
         // add velocity to move the player to either the roof or floor
-        m_YVelocity = transform.up.y * m_FlipSpeed * Time.deltaTime;
+        m_YVelocity = transform.up.y * m_FlipSpeed;
 
         // flip the player over
         transform.Rotate(transform.eulerAngles.x + 180, 0, 0);
@@ -39,7 +54,9 @@ public class PlayerMovement : MonoBehaviour {
     // add a constant forward velocity to the player
     void MoveForward()
     {
-        m_Body.velocity = new Vector2(m_ForwardSpeed * Time.deltaTime, m_YVelocity);
+        Debug.Log(m_Body.velocity);
+        m_Body.velocity = new Vector2(m_ForwardSpeed, m_YVelocity) * Time.deltaTime;
+        Debug.Log(m_Body.velocity);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
